6998e0935d0ba1d0f83ab83d1484c809e97e80f6
SamSabz <samsabz@live.com>
2021-09-26 20:15:01 +0330

    base

---
+++ config/theme.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['cache_dir'] = APPPATH . 'cache/theme';
+$config['enable_cache'] = true;
---
+++ libraries/Theme.php
@@ -0,0 +1,16 @@
+<?php
+
+class Theme {
+
+    protected $_l_delimiter = '{';
+    protected $_r_delimiter = '}';
+    protected $_l_delimiter_pattern = '';
+    protected $_r_delimiter_pattern = '';
+    protected $_cache_file;
+    protected $_cache_dir;
+    protected $_cache_path;
+    protected $_enable_cache;
+    public $CI;
+
+
+}
